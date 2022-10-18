40710c99c993b0b56b1d50be0ef53d98f48f87ef
Nicolas Ferro <nicoferro91@gmail.com>
2022-10-18 18:13:45 -0300

    error ubicacion calculo

--- desafio-clase-28/server.js
+++ desafio-clase-28/server.js
@@ -7,7 +7,7 @@ const cp = require("cookie-parser")
 
 const { argv, platform, version, memoryUsage, cwd, pid, execPath } = process
 const { fork } = require("child_process")
-const calculoPesado = require("./src/utils/calculo")
+const calculoPesado = require("./utils/calculo")
 
 const app = express();
 
@@ -175,7 +175,7 @@ app.get("/info", (req, res) => {
 app.get("/api/randoms", (req, res) => {
 	let { cant } = req.query
 	console.log(cant)
-	const random = fork("./src/utils/calculo", [cant])
+	const random = fork("./utils/calculo", [cant])
 	random.send("start")
 	random.on("message", obj => {
 		res.json(obj)
